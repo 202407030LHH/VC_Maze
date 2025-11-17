1b74b5f1cab630c96dbfaab30fd273b4fa2adaf2
202407030LHH <super8114@yuhan.ac.kr>
2025-11-17 09:07:38 +0900

    초기 데이터 세팅 및 작업 하던거 복사했음

---
+++ mazeGame_project/mazeGame_project/mazeGame_project.cpp
@@ -0,0 +1,328 @@
+﻿// mazeGame_project.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "mazeGame_project.h"
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: 여기에 코드를 입력합니다.
+
+	// 전역 문자열을 초기화합니다.
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_MAZEGAMEPROJECT, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// 애플리케이션 초기화를 수행합니다:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MAZEGAMEPROJECT));
+
+	MSG msg;
+
+	// 기본 메시지 루프입니다:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MAZEGAMEPROJECT));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_MAZEGAMEPROJECT);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+#define MAZE_ROWS 20
+#define MAZE_COLS 20
+#define MAZE_BOX_SIZE 30
+
+/// g_me = 캐릭터, g_maze_TF = 벽인지 아닌지 여부 확인
+RECT g_me;
+BOOL g_maze_TF[MAZE_ROWS][MAZE_COLS];
+
+/// x와 y좌표 전역변수화
+int g_me_x, g_me_y;
+
+/// 0 : 벽/검정색 , 1 : 길/흰색
+//int g_maze[MAZE_ROWS][MAZE_COLS] = {
+//	//   0  1  2  3  4  (Col Index)
+//	{  1, 1, 1, 1, 1 }, // Row 0 (도착지: [0][0])
+//	{  0, 0, 0, 0, 1 }, // Row 1
+//	{  0, 0, 1, 0, 1 }, // Row 2
+//	{  0, 1, 1, 0, 1 }, // Row 3
+//	{  0, 0, 0, 1, 3 }  // Row 4 (출발지: [4][4])
+//};
+
+/// 맵 크기 키워서 테스트
+int g_maze[MAZE_ROWS][MAZE_COLS] = {
+	// 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19
+	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Row 0 (End)
+	{ 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 }, // Row 1
+	{ 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 }, // Row 2
+	{ 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0 }, // Row 3
+	{ 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0 }, // Row 4
+	{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0 }, // Row 5
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0 }, // Row 6
+	{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0 }, // Row 7
+	{ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 }, // Row 8
+	{ 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0 }, // Row 9
+	{ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0 }, // Row 10
+	{ 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0 }, // Row 11
+	{ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0 }, // Row 12
+	{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0 }, // Row 13
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0 }, // Row 14
+	{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0 }, // Row 15
+	{ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Row 16
+	{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, // Row 17
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // Row 18
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3 }  // Row 19 (Start)
+};
+
+// 우선 순위 과제
+// 벽에 닿았을 때 움직이지 않게 하기
+// 현재 생각중인 방법 
+// 1. g_me의 좌표를 저장해두고 키보드 입력 시 길이 아닌 경우 break; 함수로 구현하기 쉬울 것 같음
+// 과제 해결
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CREATE:
+	{
+		g_me = { 0,0,MAZE_BOX_SIZE,MAZE_BOX_SIZE };
+		g_me_x = MAZE_ROWS-1;
+		g_me_y = MAZE_COLS - 1;
+	}
+	break;
+
+	/// 구현 로직
+	/// 이동할 좌표의 값을 임시변수 nx, ny에 할당
+	/// 키보드의 값을 입력 받을 경우, 이동할 칸에 할당된 변수의 값(0,1)을 비교
+	/// 길이라면 이동 후, 임시변수의 값을 변경.
+	/// 로직 제작 시 오래 걸린 이유
+	/// 칸에 할당된 변수와 캐릭터의 값을 비교하는 로직을 작성하지 못함
+	/// x,y 좌표 이해를 잘 하지 못했음.
+	
+	case WM_KEYDOWN:
+	{
+		int nx = g_me_x;
+		int ny = g_me_y;
+		if (wParam == VK_LEFT)
+		{
+			ny--;
+			if (ny >= 0)
+			{
+				if (g_maze[nx][ny] == 0) break;
+				g_me.left -= MAZE_BOX_SIZE;
+				g_me.right -= MAZE_BOX_SIZE;
+				g_me_y = ny;
+			}
+		}
+		else if (wParam == VK_RIGHT)
+		{
+			ny++;
+			if (ny <= MAZE_COLS - 1)
+			{
+				if (g_maze[nx][ny] == 0) break;
+				g_me.right += MAZE_BOX_SIZE;
+				g_me.left += MAZE_BOX_SIZE;
+				g_me_y = ny;
+			}
+		}
+		else if (wParam == VK_UP)
+		{
+			nx--;
+			if (nx >= 0)
+			{
+				if (g_maze[nx][ny] == 0) break;
+				g_me.top -= MAZE_BOX_SIZE;
+				g_me.bottom -= MAZE_BOX_SIZE;
+				g_me_x = nx;
+			}
+		}
+		else if (wParam == VK_DOWN)
+		{
+			nx++;
+			if (nx <= MAZE_ROWS - 1)
+			{
+				if (g_maze[nx][ny] == 0)break;
+				g_me.top += MAZE_BOX_SIZE;
+				g_me.bottom += MAZE_BOX_SIZE;
+				g_me_x = nx;
+			}
+		}
+		/// WM_PAINT를 호출
+		InvalidateRect(hWnd, NULL, FALSE);
+	}
+	break;
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// 메뉴 선택을 구문 분석합니다:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+		HBRUSH WallBrush = CreateSolidBrush(RGB(0, 0, 0));
+		HBRUSH RordBrush = CreateSolidBrush(RGB(255, 255, 255));;
+		HBRUSH CurrentBrush;
+
+		/// 맵을 생성하기 위한 코드
+		for (int i = 0; i < MAZE_ROWS; i++)
+		{
+			for (int j = 0; j < MAZE_COLS; j++)
+			{
+				/// 왜 이렇게 되는지는 다시 이해해야함.
+				int x = j * MAZE_BOX_SIZE;
+				int y = i * MAZE_BOX_SIZE;
+
+				/// 브러쉬 선택 
+				if (g_maze[i][j] == 0) CurrentBrush = WallBrush;
+				else CurrentBrush = RordBrush;
+
+				SelectObject(hdc, CurrentBrush);
+				Rectangle(hdc, x, y, x + MAZE_BOX_SIZE, y + MAZE_BOX_SIZE);
+
+				if (g_maze[i][j] == 3) {
+					SelectObject(hdc, RordBrush);
+					Ellipse(hdc, g_me.left + x, g_me.top + x, g_me.right + x, g_me.bottom + x);
+				}
+			}
+		}
+		DeleteObject(WallBrush);
+		DeleteObject(RordBrush);
+		DeleteObject(CurrentBrush);
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
